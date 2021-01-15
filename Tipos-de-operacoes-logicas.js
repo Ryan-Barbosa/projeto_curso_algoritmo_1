3f04656cc76d04ec072f6c10ad6369030308b1ec
Ryan Barbosa <ryanmatheus252002@gmail.com>
2021-01-15 15:15:31 -0300

    Primeiro commite do codigo

---
+++ Tipos-de-operacoes-logicas.js
@@ -0,0 +1,39 @@
+/*
+   passou := falso
+   escreval("Digite o nome do aluno: ")
+   leia(nome)
+   escreval("Digite a nota 01 do aluno: ")
+   leia(nota01)
+   escreval("Digite nota 02 do aluno: ")
+   leia(nota02)
+   media := (nota01 + nota02) / 2
+
+   se media >= 5 entao
+      passou := verdadeiro
+   fimse
+
+   se (passou) && ( media >=5 || media <=7) entao
+    escreval("Aprovado!", nome)
+   senao
+    escreval("Reprovado!", nome)
+
+
+   fimse
+*/
+
+var nome, nota01, nota02, passou;
+
+nome = prompt("Digite o nome do aluno:")
+nota01 = prompt("Digite a nota 01 do aluno:")
+nota02 = prompt("Digite a nota 02 do aluno:")
+passou = false
+
+media = (parseInt(nota01) + parseInt(nota02)) / 2
+
+if(media >= 5)
+    passou = true
+
+if(passou && (media >= 70 && media <= 90))
+    alert(nome + " voce foi aprovado!")
+else
+    alert(nome + " voce foi reprovado!")
